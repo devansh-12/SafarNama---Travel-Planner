8643a96976c362cc3e4d68e4bfdb0e7dc795b25f
devansh-12 <devansh.unofficial2345@gmail.com>
2025-07-19 02:06:46 +0530

    Initial commit

---
+++ auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+});
